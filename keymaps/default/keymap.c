592933c68ecca092be66c9284a41dc34daf5e827
Akshay <akshay7924@gmail.com>
2024-10-31 20:24:13 +0530

    suggested changes for shorty0

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,14 +1,18 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
-
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MS_WH_DOWN,KC_MS_WH_UP)  }
+    //                  Encoder 1                                     Encoder 2
+};
+#endif
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
     [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+        KC_P7,   KC_P8,   KC_P9,    
+        KC_P4,   KC_P5,   KC_P6,     
+        KC_P1,   KC_P2,   KC_P3,  
+        KC_A,    KC_NO,   KC_NO, // encoder button1
+        KC_D,    KC_NO,   KC_NO  // encoder button2
     )
 };
